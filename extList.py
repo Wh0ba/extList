73d4ab381c09cdf382ef0e0dcdab7b9a4dacc750
Wh0ba <whappfanr@yahoo.com>
2018-09-18 12:56:14 +0300

    now checks if the directory exists

--- extList.py
+++ extList.py
@@ -5,12 +5,23 @@ from os import listdir
 from os.path import isfile, join
 import os
 
-
+mypath = os.path.dirname(os.path.abspath(__file__))
 
 extDict = dict()
+if len(sys.argv) > 1 :
+	
+	#tmpPath = os.path.realpath(sys.argv[1])
+	if os.path.exists(os.path.realpath(sys.argv[1])):
+		mypath = os.path.realpath(sys.argv[1])
+	else:
+		#mypath = os.path.expanduser("~")
+		print "failed to get directory \nSeraching the current directory..."
+		mypath = os.path.dirname(os.path.abspath(__file__))
+else:
+	print "Seraching the current directory..."
 
 try:
-	mypath = os.path.realpath(sys.argv[1])
+	
 	filesindir = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 	
 	for filee in filesindir:
