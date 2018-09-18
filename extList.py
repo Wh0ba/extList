d3cc908bb83ed49c7057cd6e6e0b3065c81c1a26
Wh0ba <whappfanr@yahoo.com>
2018-09-18 12:35:24 +0300

    better handling for no argument launching

--- extList.py
+++ extList.py
@@ -5,11 +5,12 @@ from os import listdir
 from os.path import isfile, join
 import os
 
-mypath = os.path.realpath(sys.argv[1])
+
 
 extDict = dict()
 
 try:
+	mypath = os.path.realpath(sys.argv[1])
 	filesindir = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 	
 	for filee in filesindir:
@@ -26,6 +27,5 @@ try:
 	for key, value in extDict.items():
 		print key, value
 except NameError:
-	print mypath
 	print "directory is not valid"
 	sys.exit()
